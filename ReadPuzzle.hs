867830bb9dbebc41a70628d0d88279aa7c1312e9
dawid-woj <dd.wojciechowski@gmail.com>
2017-05-25 20:26:04 +0200

    Podstawowa wersja z nawrotami - dzialajaca, ale wolna...

--- ReadPuzzle.hs
+++ ReadPuzzle.hs
@@ -2,9 +2,11 @@
 -- kropka (.) oznacza puste pole
 -- opis lamiglowki: Plaster ["pola 1. wiersza", "pola 2. wiersza", ..., "pola n-tego wiersza"]
 -- np. Plaster ["BD..", ".GA.D", ".FEG", "ABDCF", "E..."]
--- dlugosc kolejnych wierszy: (n-1), n, (n-1), ..., (n-1)
+-- dlugosc kolejnych wierszy: (n-1), n, (n-1), ..., (n-1) => nieparzysta liczba wierszy planszy
 --
 -- obrane zalozenie: minimalna obslugiwana plansza -> 3-wierszowa
+
+-- ReadPuzzle - modul czytania lamiglowki z pliku
 
 module ReadPuzzle
 ( Plaster(Plaster)
@@ -14,9 +16,10 @@ module ReadPuzzle
 
 import System.IO
 
-data Plaster = Plaster [String] deriving (Read, Show)
+data Plaster = Plaster [String] deriving (Read, Show) --typ reprezentujacy plansze lamiglowki
 type Filename = String
 
+--readPuzzle (nazwa_pliku) -> odczytuje plik o danej nazwie i zwraca plansze lamiglowki (lub blad)
 readPuzzle :: Filename -> IO Plaster
 readPuzzle [] = error "Empty file name!"
 readPuzzle name = do
@@ -24,13 +27,7 @@ readPuzzle name = do
 	let puzzle = (read content) :: Plaster
 	return puzzle
 
-checkPuzzle' :: Int -> Int -> [String] -> Bool
-checkPuzzle' _ _ [] = True
-checkPuzzle' n s (r:rs)
-	| n-s /= length r = False
-	| not (all (\a -> any (==a) ".ABCDEFG") r) = False
-	| otherwise = if s == 0 then checkPuzzle' n 1 rs else checkPuzzle' n 0 rs
-
+--checkPuzzle (plansza) -> true jesli dana plansza jest poprawna pod wzgledem formy i zawartosci
 checkPuzzle :: Plaster -> Bool
 checkPuzzle (Plaster []) = False
 checkPuzzle (Plaster rows)
@@ -38,4 +35,10 @@ checkPuzzle (Plaster rows)
 	| n `mod` 2 == 0 = False
 	| otherwise = checkPuzzle' n 1 rows
 	where n = length rows
+checkPuzzle' :: Int -> Int -> [String] -> Bool
+checkPuzzle' _ _ [] = True
+checkPuzzle' n s (r:rs)
+	| n-s /= length r = False
+	| not (all (\a -> any (==a) ".ABCDEFG") r) = False
+	| otherwise = if s == 0 then checkPuzzle' n 1 rs else checkPuzzle' n 0 rs
 
--- main.hs
+++ main.hs
@@ -2,84 +2,81 @@
 -- kropka (.) oznacza puste pole
 -- opis lamiglowki: Plaster ["pola 1. wiersza", "pola 2. wiersza", ..., "pola n-tego wiersza"]
 -- np. Plaster ["BD..", ".GA.D", ".FEG", "ABDCF", "E..."]
--- dlugosc kolejnych wierszy: (n-1), n, (n-1), ..., (n-1)
---
--- wymog glowny: zeby nie robic tego w sposob brutalny, tzn. generowac wszystkich mozliwych rozwiazan
+-- dlugosc kolejnych wierszy: (n-1), n, (n-1), ..., (n-1) => nieparzysta liczba wierszy planszy
 --
 -- obrane zalozenie: minimalna obslugiwana plansza -> 3-wierszowa
 
 import ReadPuzzle
-import Data.List --sort, find, elemindex, elemIndices, \\ operator, concat, nub
-import Data.Maybe --fromJust
 
+import Data.List --sort, find, elemindex, elemIndices, \\ operator, concat
+import Data.Maybe --fromJust
+import Debug.Trace --trace
+
+--pozycja w planszy (wiersz, kolumna); indeksowanie od zera:
 type Pos = (Int,Int)
 
-noCumb = 'X'
-emptyCumb = '.'
+_NO_CUMB = 'X'		-- "brak" plastra w planszy (oznaczenie granicy planszy)
+_EMPTY_CUMB = '.'	-- pusty plaster planszy
 
+--typ aktualnego stanu planszy (rozwiazania):
 data PuzzleState = PuzzleState {
 puzzle :: Plaster,				-- plansza
 emptyCumbs :: Int,				-- liczba pustych plastrow pozostalych do rozpatrzenia
 status :: Bool					-- status zwracany przez kazdy krok algorytmu -> false: nieudalo sie / nawrot, true: udalo sie, idziemy dalej
-} 
+} deriving Show
 
---PuzzleState -> czy plansza jest wypelniona w calosci?
+--isPuzzleFilled (stan_planszy) -> true jesli plansza jest wypelniona
 isPuzzleFilled :: PuzzleState -> Bool
 isPuzzleFilled p = (emptyCumbs p) == 0
 
---Plaster -> liczba pustych plastrow w planszy
+--countEmptyCumbs (plansza) -> liczba pustych plastrow w planszy
 countEmptyCumbs :: Plaster -> Int
 countEmptyCumbs (Plaster []) = 0
-countEmptyCumbs (Plaster (r:rs)) = length (elemIndices emptyCumb r) + countEmptyCumbs (Plaster rs)
+countEmptyCumbs (Plaster (r:rs)) = length (elemIndices _EMPTY_CUMB r) + countEmptyCumbs (Plaster rs)
 
--- Plaster pos -> czy sasiedzi plastra na pozycji 'pos' sa odpowiedni?
+--checkNeighbours (plansza) (pozycja) -> czy sasiedzi plastra na danej pozycji sa odpowiedni (maja rozne wartosci)?
 checkNeighbours :: Plaster -> Pos -> Bool
 checkNeighbours p pos = not (isThereDuplicates (sort (getCumb p pos : getNearCumbs p pos)))
 isThereDuplicates :: String -> Bool
 isThereDuplicates [] = False
 isThereDuplicates [x] = False
 isThereDuplicates (x:y:ys)
-	| x == y && x /= noCumb && x /= emptyCumb = True
+	| x == y && x /= _NO_CUMB && x /= _EMPTY_CUMB = True
 	| otherwise = isThereDuplicates (y:ys)
 
--- Plaster pos -> lista wartosci plastrow sasiednich dla plastra na pozycji 'pos'
+--getNearCumbs (plansza) (pozycja) -> lista wartosci plastrow sasiednich dla plastra na danej pozycji
 getNearCumbs :: Plaster -> Pos -> String
 getNearCumbs (Plaster []) _ = []
 getNearCumbs p (r,c)
 	| r `mod` 2 /= 0 = [getCumb p (x,y) | (x,y) <- [(r-1,c-1), (r-1,c), (r,c-1), (r,c+1), (r+1,c-1), (r+1,c)]]
 	| otherwise = [getCumb p (x,y) | (x,y) <- [(r-1,c), (r-1,c+1), (r,c-1), (r,c+1), (r+1,c), (r+1,c+1)]]
 
---checkNeighbours2 :: Plaster -> Pos -> Bool
---checkNeighbours2 p pos = not (isThereDuplicates (sort (getCumb p pos : getNearCumbs p pos)))
+--getNearCumbs2 :: Plaster -> Pos -> String
+--getNearCumbs2 (Plaster []) _ = []
+--getNearCumbs2 p (r,c)
+--	| r `mod` 2 /= 0 = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c-1), (r-1,c), (r,c-1), (r,c+1), (r+1,c-1), (r+1,c)]]) \\ (replicate 6 (getCumb p (r,c)))
+--	| otherwise = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c), (r-1,c+1), (r,c-1), (r,c+1), (r+1,c), (r+1,c+1)]]) \\ (replicate 6 (getCumb p (r,c)))
 
-getNearCumbs2 :: Plaster -> Pos -> String
-getNearCumbs2 (Plaster []) _ = []
-getNearCumbs2 p (r,c)
-	| r `mod` 2 /= 0 = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c-1), (r-1,c), (r,c-1), (r,c+1), (r+1,c-1), (r+1,c)]]) \\ (replicate 6 (getCumb p (r,c)))
-	| otherwise = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c), (r-1,c+1), (r,c-1), (r,c+1), (r+1,c), (r+1,c+1)]]) \\ (replicate 6 (getCumb p (r,c)))
-
--- Plaster pos -> wartosc plastra na pozycji 'pos'
+--getCumb (plansza) (pozycja) -> wartosc plastra na danej pozycji
 getCumb :: Plaster -> Pos -> Char
 getCumb (Plaster rows) (r,c)
-	| r < 0 || r >= length rows = noCumb
-	| c < 0 || c >= length (rows !! r) = noCumb
+	| r < 0 || r >= length rows = _NO_CUMB
+	| c < 0 || c >= length (rows !! r) = _NO_CUMB
 	| otherwise = (rows !! r) !! c
 
---Plaster pos -> pozycja kolejnego pustego plastra zaczynajac poszukiwania od pozycji 'pos'
+--findNextEmptyCumb (plansza) (pozycja) -> pozycja kolejnego pustego plastra zaczynajac poszukiwania od danje pozycji lub Nothing, gdy nie ma
 findNextEmptyCumb :: Plaster -> Pos -> Maybe Pos
 findNextEmptyCumb (Plaster []) _ = Nothing
 findNextEmptyCumb (Plaster rows) (r,c)
 	| r >= len = Nothing
-	| id == Nothing = findNextEmptyCumb (Plaster rows) (r',c')
+	| id == Nothing = findNextEmptyCumb (Plaster rows) (r+1,-1)
 	| otherwise = Just (r, fromJust id)
 	where
 		len = length rows
-		r' = if c == length row - 1 then r+1 else r
-		c' = if c == length row - 1 then 0 else c+1
-		id = find (\x -> x > c) (elemIndices emptyCumb row)
+		id = find (\x -> x > c) (elemIndices _EMPTY_CUMB row)
 		row = rows !! r
 
---replElem lista id v -> lista po zmianie, czyli zamiany elementu list na pozycji 'id' na wartosc 'v'
+--replElem (lista) (index_elementu_do_zamiany) (nowa_wartosc) -> lista po zmianie, czyli zamiany elementu listy pod danym indeksem na nowa wartosc
 replElem :: [a] -> Int -> a -> [a]
 replElem [] _ _ = []
 replElem (x:xs) id v
@@ -87,56 +84,54 @@ replElem (x:xs) id v
 	| id == 0 = v:xs
 	| otherwise = x : replElem xs (id-1) v
 
---Plaster pos c -> plansza po zmianie, czyli wstawieniu 'c' w plaster na pozycji 'pos'
---(nie sprawdza poprawnosci 'pos' i tego co bylo w plastrze przed wstawieniem!)
+--fillCumb (plansza) (pozycja) (wartosc) -> plansza po zmianie, czyli wstawieniu danej wartosci w plaster na danej pozycji
+--(nie sprawdza poprawnosci pozycji i tego co bylo w plastrze przed wstawieniem!)
 fillCumb :: Plaster -> Pos -> Char -> Plaster
 fillCumb (Plaster []) _ _ = (Plaster [])
 fillCumb (Plaster rows) (r,c) v = (Plaster (replElem rows r row))
 	where row = replElem (rows !! r) c v
 
---Plaster -> plansza po rozwiazaniu
+--solvePuzzle (plansza) -> plansza po rozwiazaniu
 solvePuzzle :: Plaster -> Plaster
 solvePuzzle (Plaster []) = (Plaster [])
 solvePuzzle p
 	| ec == 0 = p
-	| otherwise = puzzle (solve (PuzzleState p ec False) pos)
+	| otherwise = puzzle (solve (PuzzleState p ec False) (Just (0,-1)))
 	where
-		pos = findNextEmptyCumb p (0,-1)
-		ec = countEmptyCumbs p
+		ec = countEmptyCumbs p		
 
---PuzzleState pos -> stan po uzupelnieniu znalezionego pustego plastra na pozycji 'pos'
+--solve (stan_planszy) (ostatnio_analizowana_pozycja) -> stan planszy po dokonanej zmianie
 solve :: PuzzleState -> Maybe Pos -> PuzzleState
-solve p Nothing
-	| emptyCumbs p > 0 || not (checkSolution (puzzle p)) = (PuzzleState (puzzle p) (emptyCumbs p) False)
-	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) True)
-solve p@(PuzzleState _ 0 _) pos = (PuzzleState (puzzle p) (emptyCumbs p) False)
-solve p pos = tryOptions p pos options
+solve p Nothing = p
+solve p pos
+--	| trace ("solve: state = " ++ show p ++ " pos = " ++ show pos) False = undefined
+	| newPos /= Nothing = tryOptions p newPos options
+	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) (checkSolution (puzzle p)))
 	where
-		nears = getNearCumbs (puzzle p) (fromJust pos)
---		nears = getNearCumbs2 (puzzle p) (fromJust pos)
-		options = "ABCDEFG" \\ nears
+		newPos = findNextEmptyCumb (puzzle p) (fromJust pos)
+		options = "ABCDEFG" \\ getNearCumbs (puzzle p) (fromJust newPos)
 
+--tryOptions (stan_planszy) (pozycja_pustego_plastra) (lista_mozliwych_wartosci_do_wstawienia) -> stan planszy po dokonanej zmianie
 tryOptions :: PuzzleState -> Maybe Pos -> String -> PuzzleState
-tryOptions p _ [] = (PuzzleState (puzzle p) (emptyCumbs p) False)
-tryOptions p Nothing _
-	| emptyCumbs p > 0 || not (checkSolution (puzzle p)) = (PuzzleState (puzzle p) (emptyCumbs p) False)
-	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) True)
+tryOptions (PuzzleState p ec _) _ [] = (PuzzleState p ec False)
+tryOptions p Nothing _ = p
 tryOptions p pos (o:os)
---	| elem o (getNearCumbs (puzzle p) (fromJust pos)) = try
-	| not (status opt) = tryOptions p pos os
-	| otherwise = opt
+--	| trace ("tryOptions: state = " ++ show p ++ " pos = " ++ show pos ++ " options = " ++ show (o:os)) False = undefined
+	| not (status next) = tryOptions p pos os
+	| not (checkSolution (puzzle next)) = tryOptions p pos os
+	| otherwise = (PuzzleState (puzzle next) (emptyCumbs next) True)
 	where
 		newPuzzle = fillCumb (puzzle p) (fromJust pos) o
 		emptyCount = (emptyCumbs p) - 1
-		newPos = findNextEmptyCumb (puzzle p) (fromJust pos)
-		opt = solve (PuzzleState newPuzzle emptyCount False) newPos
+		next = solve (PuzzleState newPuzzle emptyCount False) pos
 
---Plaster -> czy dana plansza jest poprawnym rozwiazaniem?
+--checkSolution (plansza) -> czy dana plansza jest poprawnym rozwiazaniem?
 checkSolution :: Plaster -> Bool
 checkSolution (Plaster rows) = checkAt (Plaster rows) (0,0)
 checkAt :: Plaster -> Pos -> Bool
 checkAt (Plaster rows) (r,c)
-	| (getCumb (Plaster rows) (r,c)) == emptyCumb || not (checkNeighbours (Plaster rows) (r,c)) = False
+--	| trace ("checkAt: (r,c) = " ++ show (r,c)) False = undefined
+	| (getCumb (Plaster rows) (r,c)) == _EMPTY_CUMB || not (checkNeighbours (Plaster rows) (r,c)) = False
 	| (r == len-1) && (c == length (rows !! r) - 1) = True
 	| otherwise = checkAt (Plaster rows) (r',c')
 	where
@@ -144,7 +139,7 @@ checkAt (Plaster rows) (r,c)
 		c' = if c == length (rows !! r) - 1 then 0 else c+1
 		len = length rows
 
---Plaster -> string do wizualnego wyswietlenia planszy
+--puzzlePrettyString (plansza) -> string do landego wyswietlenia planszy w terminalu
 puzzlePrettyString :: Plaster -> String
 puzzlePrettyString (Plaster rows) = puzzlePrettyString' (Plaster rows) (length rows)
 puzzlePrettyString' :: Plaster -> Int -> String
