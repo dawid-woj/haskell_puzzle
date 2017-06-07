35e10fbac97899a284bbfed31ef7e2ac0541ae92
sgalazka <szymon.galazka93@gmail.com>
2017-06-07 11:25:31 +0200

    optymalizacja, sortowanie pustych pol wzgledem liczby sasiadow, badanie sasiadow sasiadow

--- main.hs
+++ main.hs
@@ -11,6 +11,8 @@ import ReadPuzzle
 import Data.List --sort, find, elemindex, elemIndices, \\ operator, concat
 import Data.Maybe --fromJust
 import Debug.Trace --trace
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 --pozycja w planszy (wiersz, kolumna); indeksowanie od zera:
 type Pos = (Int,Int)
@@ -44,18 +46,29 @@ isThereDuplicates (x:y:ys)
 	| x == y && x /= _NO_CUMB && x /= _EMPTY_CUMB = True
 	| otherwise = isThereDuplicates (y:ys)
 
---getNearCumbs (plansza) (pozycja) -> lista wartosci plastrow sasiednich dla plastra na danej pozycji
+--getNearCumbs (plansza) (pozycja) -> lista wartosci plastrow sąsiadów i sąsiadów sąsiadów dla plastra na danej pozycji
 getNearCumbs :: Plaster -> Pos -> String
 getNearCumbs (Plaster []) _ = []
 getNearCumbs p (r,c)
+	| r `mod` 2 == 0 =  [getCumb p (x,y) | (x,y) <- [(r-2,c-1), (r-2,c), (r-2,c+1),
+																	   (r-1,c-1), (r-1,c),(r-1,c+1), (r-1,c+2), 
+																	(r,c-2), (r,c-1), (r,c), (r,c+1), (r,c+2), 
+																	   (r+1,c-1), (r+1,c),(r+1,c+1), (r+1,c+2),
+																	        (r+2,c-1), (r+2,c),(r+2,c+1)
+																	 ]]
+	| otherwise = [getCumb p (x,y) | (x,y) <- [       (r-2,c-1), (r-2,c), (r-2,c+1),
+																	   (r-1,c-2), (r-1,c-1),(r-1,c), (r-1,c+1), 
+																	(r,c-2), (r,c-1), (r,c), (r,c+1), (r,c+2), 
+																	   (r+1,c-2), (r+1,c-1),(r+1,c), (r+1,c+1),
+																	        (r+2,c-1), (r+2,c),(r+2,c+1)
+																	 ]]
+
+--getNearCumbs1 (plansza) (pozycja) -> lista wartosci plastrow sasiednich dla plastra na danej pozycji
+getNearCumbs1 :: Plaster -> Pos -> String
+getNearCumbs1 (Plaster []) _ = []
+getNearCumbs1 p (r,c)
 	| r `mod` 2 /= 0 = [getCumb p (x,y) | (x,y) <- [(r-1,c-1), (r-1,c), (r,c-1), (r,c+1), (r+1,c-1), (r+1,c)]]
 	| otherwise = [getCumb p (x,y) | (x,y) <- [(r-1,c), (r-1,c+1), (r,c-1), (r,c+1), (r+1,c), (r+1,c+1)]]
-
---getNearCumbs2 :: Plaster -> Pos -> String
---getNearCumbs2 (Plaster []) _ = []
---getNearCumbs2 p (r,c)
---	| r `mod` 2 /= 0 = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c-1), (r-1,c), (r,c-1), (r,c+1), (r+1,c-1), (r+1,c)]]) \\ (replicate 6 (getCumb p (r,c)))
---	| otherwise = (concat [getNearCumbs p (x,y) | (x,y) <- [(r-1,c), (r-1,c+1), (r,c-1), (r,c+1), (r+1,c), (r+1,c+1)]]) \\ (replicate 6 (getCumb p (r,c)))
 
 --getCumb (plansza) (pozycja) -> wartosc plastra na danej pozycji
 getCumb :: Plaster -> Pos -> Char
@@ -95,35 +108,48 @@ fillCumb (Plaster rows) (r,c) v = (Plaster (replElem rows r row))
 solvePuzzle :: Plaster -> Plaster
 solvePuzzle (Plaster []) = (Plaster [])
 solvePuzzle p
+	| trace("solvePuzzle, emptyPositions: "++show emptyPositions) False = undefined
 	| ec == 0 = p
-	| otherwise = puzzle (solve (PuzzleState p ec False) (Just (0,-1)))
+	| otherwise = puzzle (solve (PuzzleState p ec False) (Just (0,-1)) emptyPositions)
 	where
-		ec = countEmptyCumbs p		
+		ec = countEmptyCumbs p
+		emptyPositions = reverse (sort (getEmptyPositions p (0, -1)))
 
 --solve (stan_planszy) (ostatnio_analizowana_pozycja) -> stan planszy po dokonanej zmianie
-solve :: PuzzleState -> Maybe Pos -> PuzzleState
-solve p Nothing = p
-solve p pos
---	| trace ("solve: state = " ++ show p ++ " pos = " ++ show pos) False = undefined
-	| newPos /= Nothing = tryOptions p newPos options
+solve :: PuzzleState -> Maybe Pos -> [EmptyPos] -> PuzzleState
+solve p Nothing _ = p
+solve p pos [] 
+	| trace ("solve: state = " ++ show p ++ " pos = " ++ show pos ++", options: []") False = undefined
+	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) (checkSolution (puzzle p)))
+solve p pos [(EmptyPos pos1 n)]
+	| trace ("solve: state = " ++ show p ++ " pos = " ++ show pos ++", options: " ++ show options) False = undefined
+	| newPos /= Nothing = tryOptions p newPos options []
 	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) (checkSolution (puzzle p)))
 	where
-		newPos = findNextEmptyCumb (puzzle p) (fromJust pos)
+		newPos = Just pos1
+		options = "ABCDEFG" \\ getNearCumbs (puzzle p) (fromJust newPos)
+	
+solve p pos ((EmptyPos pos1 n):es)
+	| trace ("solve: state = " ++ show p ++ " pos = " ++ show pos ++ ", options: " ++ show options) False = undefined
+	| newPos /= Nothing = tryOptions p newPos options es
+	| otherwise = (PuzzleState (puzzle p) (emptyCumbs p) (checkSolution (puzzle p)))
+	where
+		newPos = Just pos1
 		options = "ABCDEFG" \\ getNearCumbs (puzzle p) (fromJust newPos)
 
 --tryOptions (stan_planszy) (pozycja_pustego_plastra) (lista_mozliwych_wartosci_do_wstawienia) -> stan planszy po dokonanej zmianie
-tryOptions :: PuzzleState -> Maybe Pos -> String -> PuzzleState
-tryOptions (PuzzleState p ec _) _ [] = (PuzzleState p ec False)
-tryOptions p Nothing _ = p
-tryOptions p pos (o:os)
+tryOptions :: PuzzleState -> Maybe Pos -> String -> [EmptyPos] -> PuzzleState
+tryOptions (PuzzleState p ec _) _ [] _ = (PuzzleState p ec False)
+tryOptions p Nothing _ _ = p
+tryOptions p pos (o:os) ep
 --	| trace ("tryOptions: state = " ++ show p ++ " pos = " ++ show pos ++ " options = " ++ show (o:os)) False = undefined
-	| not (status next) = tryOptions p pos os
-	| not (checkSolution (puzzle next)) = tryOptions p pos os
+	| not (status next) = tryOptions p pos os ep
+	| not (checkSolution (puzzle next)) = tryOptions p pos os ep
 	| otherwise = (PuzzleState (puzzle next) (emptyCumbs next) True)
 	where
 		newPuzzle = fillCumb (puzzle p) (fromJust pos) o
 		emptyCount = (emptyCumbs p) - 1
-		next = solve (PuzzleState newPuzzle emptyCount False) pos
+		next = solve (PuzzleState newPuzzle emptyCount False) pos ep
 
 --checkSolution (plansza) -> czy dana plansza jest poprawnym rozwiazaniem?
 checkSolution :: Plaster -> Bool
@@ -152,11 +178,12 @@ puzzlePrettyRow [] = []
 puzzlePrettyRow (x:xs) = x : " " ++ puzzlePrettyRow xs
 
 
-main = do
+main = do 
 	putStrLn "Welcome to Honeycomb Puzzle!"
 	putStrLn "Give the name of puzzle level to solve: "
-	filename <- getLine
-	puzzle <- readPuzzle filename
+--	filename <- getLine
+-- TODO odkomentowac stale dodawanie planszy	
+	puzzle <- readPuzzle "plansze/latwe1.txt"
 	--putStrLn ("Puzzle: " ++ show puzzle)
 	if (checkPuzzle puzzle) then do
 		putStrLn "Puzzle form and content are valid."
@@ -167,3 +194,78 @@ main = do
 		putStr (puzzlePrettyString solution)
 	else
 		putStrLn "Invalid form/content of puzzle!"
+
+
+
+data EmptyPos = EmptyPos {
+pos :: Pos,
+neighboursCount :: Int
+} 
+
+instance Eq EmptyPos where
+    (EmptyPos _ c1) == (EmptyPos _ c2) = c1 == c2
+  
+instance Ord EmptyPos where 
+    (EmptyPos _ c1) `compare` (EmptyPos _ c2) = c1 `compare` c2
+	
+instance Show EmptyPos where
+	show (EmptyPos (r,c) n) = "{("++show r++", "++show c++"), "++show n++"}"
+
+getEmptyPositions :: Plaster -> Pos -> [EmptyPos]
+getEmptyPositions (Plaster rows) (r, c) 
+    | (r > len-1)  = []
+	| trace ("getEmptyPositions, row:"++ show r ++ ", column: " ++ show c ++ ", cumb: "++ show cumb ++ ", c: '"++show c'++ ", r': "++show r') False = undefined
+    | cumb == _EMPTY_CUMB = (EmptyPos (r,c) neighboursCount) : getEmptyPositions (Plaster rows) (r',c')
+	| otherwise =  getEmptyPositions (Plaster rows) (r',c')
+    where
+		r' = if c == length (rows !! r) - 1 then r+1 else r
+		c' = if c == length (rows !! r) - 1 then 0 else c+1
+		cumb = (getCumb (Plaster rows) (r, c))
+		len = length rows
+		neighboursCount = length ([n | n <- getNearCumbs1 (Plaster rows) (r,c), n /= _EMPTY_CUMB && n /= _NO_CUMB])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
